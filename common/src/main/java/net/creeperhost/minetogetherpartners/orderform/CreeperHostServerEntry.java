ae08479b38c5ebfdb9b434af6ee96c2b760c732a
brandon3055 <brandon4261@gmail.com>
2025-02-08 14:39:50 +1100

    1.20.4 port

--- common/src/main/java/net/creeperhost/minetogetherpartners/orderform/CreeperHostServerEntry.java
+++ common/src/main/java/net/creeperhost/minetogetherpartners/orderform/CreeperHostServerEntry.java
@@ -19,9 +19,9 @@ import net.minecraft.resources.ResourceLocation;
 public class CreeperHostServerEntry extends ServerSelectionList.NetworkServerEntry {
 
     private final Minecraft mc = Minecraft.getInstance();
-    private final ResourceLocation serverIcon = ResourceLocation.fromNamespaceAndPath(MineTogetherPartners.MOD_ID, "textures/creeperhost.png");
+    private final ResourceLocation serverIcon = new ResourceLocation(MineTogetherPartners.MOD_ID, "textures/creeperhost.png");
     private float transparency = 0.5F;
-    protected final ResourceLocation BUTTON_TEXTURES = ResourceLocation.fromNamespaceAndPath(MineTogetherPartners.MOD_ID, "textures/hidebtn.png");
+    protected final ResourceLocation BUTTON_TEXTURES = new ResourceLocation(MineTogetherPartners.MOD_ID, "textures/hidebtn.png");
     private ServerSelectionList serverSelectionList;
     private Button removeButton;
 
